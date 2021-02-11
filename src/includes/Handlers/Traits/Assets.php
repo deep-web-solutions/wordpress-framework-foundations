3d74cc9e20436c106d27d04147f6d5db1f14fe26
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-02-11 18:49:08 +0100

    improved traits

--- src/includes/Handlers/Traits/Assets.php
+++ src/includes/Handlers/Traits/Assets.php
@@ -4,6 +4,7 @@ namespace DeepWebSolutions\Framework\Utilities\Handlers\Traits;
 
 use DeepWebSolutions\Framework\Helpers\WordPress\Traits\Assets as AssetsHelpers;
 use DeepWebSolutions\Framework\Utilities\Handlers\AssetsHandler;
+use DeepWebSolutions\Framework\Utilities\Interfaces\Identifiable;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -16,7 +17,9 @@ defined( 'ABSPATH' ) || exit;
  * @package DeepWebSolutions\WP-Framework\Utilities\Handlers\Traits
  */
 trait Assets {
-	use AssetsHelpers;
+	use AssetsHelpers {
+		get_asset_handle as get_asset_handle_helpers;
+	}
 
 	/**
 	 * Using classes should define their assets in here.
@@ -27,4 +30,30 @@ trait Assets {
 	 * @param   AssetsHandler   $assets_handler      Instance of the hooks handler.
 	 */
 	abstract protected function enqueue_assets( AssetsHandler $assets_handler ): void;
+
+	/**
+	 * Returns a meaningful, hopefully unique, handle for an asset.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @see     AssetsHelpers::get_asset_handle()
+	 *
+	 * @param   string  $name   The actual descriptor of the asset's purpose. Leave blank for default.
+	 * @param   array   $extra  Further descriptor of the asset's purpose.
+	 * @param   string  $root   Prepended to all asset handles inside the same class.
+	 *
+	 * @return  string
+	 */
+	public function get_asset_handle( string $name = '', array $extra = array(), string $root = '' ): string {
+		if ( $this instanceof Identifiable ) {
+			return $this->get_asset_handle_helpers(
+				$name,
+				$extra,
+				$this->get_plugin()->get_plugin_slug() . '_' . ( $root ?: $this->get_instance_public_name() ) // phpcs:ignore
+			);
+		}
+
+		return $this->get_asset_handle_helpers( $name, $extra, $root );
+	}
 }
--- src/includes/Handlers/Traits/Hooks.php
+++ src/includes/Handlers/Traits/Hooks.php
@@ -4,6 +4,7 @@ namespace DeepWebSolutions\Framework\Utilities\Handlers\Traits;
 
 use DeepWebSolutions\Framework\Helpers\WordPress\Traits\Hooks as HooksHelpers;
 use DeepWebSolutions\Framework\Utilities\Handlers\HooksHandler;
+use DeepWebSolutions\Framework\Utilities\Interfaces\Identifiable;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -16,7 +17,9 @@ defined( 'ABSPATH' ) || exit;
  * @package DeepWebSolutions\WP-Framework\Utilities\Handlers\Traits
  */
 trait Hooks {
-	use HooksHelpers;
+	use HooksHelpers {
+		get_hook_name as get_hook_name_helpers;
+	}
 
 	/**
 	 * Using classes should define their hooks in here.
@@ -27,4 +30,30 @@ trait Hooks {
 	 * @param   HooksHandler    $hooks_handler      Instance of the hooks handler.
 	 */
 	abstract protected function register_hooks( HooksHandler $hooks_handler ): void;
+
+	/**
+	 * Returns a meaningful, hopefully unique, name for an internal hook.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @see     HooksHelpers::get_hook_name()
+	 *
+	 * @param   string  $name       The actual descriptor of the hook's purpose.
+	 * @param   array   $extra      Further descriptor of the hook's purpose.
+	 * @param   string  $root       Prepended to all hooks inside the same class.
+	 *
+	 * @return  string
+	 */
+	public function get_hook_name( string $name, array $extra = array(), string $root = '' ): string {
+		if ( $this instanceof Identifiable ) {
+			return $this->get_hook_name_helpers(
+				$name,
+				$extra,
+				$this->get_plugin()->get_plugin_slug() . '_' . ( $root ?: $this->get_instance_public_name() ) // phpcs:ignore
+			);
+		}
+
+		return $this->get_hook_name_helpers( $name, $extra, $root );
+	}
 }

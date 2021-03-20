78eb8c47b2a7e0124d6525f90f60442fcf65a549
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-03-20 14:50:59 +0100

    Added more default stores

--- src/includes/PluginUtilities/Storage/Stores/OptionsStore.php
+++ src/includes/PluginUtilities/Storage/Stores/OptionsStore.php
@@ -7,17 +7,17 @@ use DeepWebSolutions\Framework\Foundations\PluginUtilities\Storage\AbstractStore
 \defined( 'ABSPATH' ) || exit;
 
 /**
- * Reusable implementation of a basic in-memory store.
+ * Reusable implementation of a basic options-table store.
  *
  * @since   1.0.0
  * @version 1.0.0
  * @author  Antonius Hegyes <a.hegyes@deep-web-solutions.com>
  * @package DeepWebSolutions\WP-Framework\Foundations\PluginUtilities\Storage\Stores
  */
-class MemoryStore extends AbstractStore {
-	// region TRAITS
+class OptionsStore extends AbstractStore {
+	// region METHODS
 
-	use MemoryStoreTrait;
+	use OptionsStoreTrait;
 
 	// endregion
 }
--- src/includes/PluginUtilities/Storage/Stores/UserMetaStore.php
+++ src/includes/PluginUtilities/Storage/Stores/UserMetaStore.php
@@ -7,17 +7,17 @@ use DeepWebSolutions\Framework\Foundations\PluginUtilities\Storage\AbstractStore
 \defined( 'ABSPATH' ) || exit;
 
 /**
- * Reusable implementation of a basic in-memory store.
+ * Reusable implementation of a basic user-meta store.
  *
  * @since   1.0.0
  * @version 1.0.0
  * @author  Antonius Hegyes <a.hegyes@deep-web-solutions.com>
  * @package DeepWebSolutions\WP-Framework\Foundations\PluginUtilities\Storage\Stores
  */
-class MemoryStore extends AbstractStore {
-	// region TRAITS
+class UserMetaStore extends AbstractStore {
+	// region METHODS
 
-	use MemoryStoreTrait;
+	use UserMetaStoreTrait;
 
 	// endregion
 }
